1f2076316e9f1d83d10d44cc55e32cd8b1ac3311
sh <s3pt3mb3r@protonmail.com>
2024-08-30 14:23:50 +0900

    add redis url and fix middleware routes

--- src/routes/auth_routes.rs
+++ src/routes/auth_routes.rs
@@ -2,13 +2,15 @@ use actix_web::{middleware::from_fn, web};
 
 use super::{handlers, middlewares};
 
-// This in charge of every path in /auth path
 pub fn config(config: &mut web::ServiceConfig) {
     config.service(
         web::scope("/auth")
             .service(handlers::auth_handlers::register)
             .service(handlers::auth_handlers::login)
-            .wrap(from_fn(middlewares::auth_middleware::check_auth_middleware))
-            .service(handlers::auth_handlers::logout),
+            .service(
+                web::scope("")
+                    .wrap(from_fn(middlewares::auth_middleware::check_auth_middleware))
+                    .service(handlers::auth_handlers::logout),
+            ),
     );
 }
